25f9ac7390501a728609b3ab8e9c8f96c137fef3
github username <github email address>
2019-06-10 10:28:13 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,88 +1,65 @@
-// [3]
-const form = document.querySelector('#new-quote-form')
-
-// [5]
-const quoteInput = document.querySelector('#new-quote')
-const authorInput = document.querySelector('#author')
-
-// [1]
 document.addEventListener('DOMContentLoaded', function() {
-  fetch_quotes()
-  form.addEventListener('submit', createQuote)
+  fetch_quote()
+  document.addEventListener('click', handleClickEvents)
+  function handleClickEvents(e) {
+    e.preventDefault()
+    if(e.target.className === "btn btn-primary") create_quote(e.target.parentElement)
+    else if(e.target.className === "btn-success") like_quote(e.target)
+    else if(e.target.className === 'btn-danger') delete_quote(e.target)
+    // console.log(e.target.parentElement)
+  }
+
+
+  function fetch_quote() {
+    return fetch('http://localhost:3000/quotes')
+    .then (res => res.json())
+    .then (json => {json.forEach(display_quote) })
+  }
+  
+  function display_quote(quote) {
+    document.querySelector('#quote-list').innerHTML += 
+    `<li class='quote-card dataset.id="${quote.id}'>
+      <blockquote class="blockquote">
+      <p class="mb-0">${quote.quote}</p>
+      <footer class="blockquote-footer">${quote.author}</footer>
+      <br>
+      <button data-quote-id="${quote.id}" class='btn-success'>Likes: <span>${quote.likes}</span></button>
+		  <button data-quote-id="${quote.id}" class='btn-danger'>Delete</button>
+      </blockquote>
+    </li>`  
+  }
+
+  function create_quote(form) {
+    fetch('http://localhost:3000/quotes',{
+			method: 'POST',
+			headers: {
+				'Content-Type':'application/json',
+				Accept: 'application/json'
+			},
+			body: JSON.stringify({ quote: form["new-quote"].value, author: form.author.value, likes: '0'
+			})
+		})
+		.then(resp => resp.json())
+		.then(display_quote)
+		form.reset()
+  }
+
+  function like_quote(button) {
+		let like_span = button.children[0]
+		like_span.innerText = parseInt(like_span.innerText)+1
+
+		fetch(`http://localhost:3000/quotes/${button.dataset.quoteId}`,{
+	    method: 'PATCH',
+	    headers: {'Content-Type':'application/json'},
+	    body: JSON.stringify({likes: like_span.innerText})
+	  })
+	}
+
+	function delete_quote(button) {
+		fetch(`http://localhost:3000/quotes/${button.dataset.quoteId}`,{
+			method: 'DELETE'
+		})
+		button.parentElement.parentElement.remove()
+	}
 
 })
-
-// [2]
-function fetch_quotes() {
-  return fetch('http://localhost:3000/quotes')
-  .then(resp => resp.json())
-  .then(json => { json.forEach(display_quote) })
-
-}
-
-// [4]
-function display_quote(quote) {
-  // console.log(quote)
-
-  const ul = document.querySelector('#quote-list')
-
-  const li = document.createElement('li')
-  li.className = 'quote-card'
-  li.dataset.id = quote.id
-
-  const blockquote = document.createElement('blockquote')
-  blockquote.className = 'blockqutoe'
-
-  const p = document.createElement('mb-0')
-  p.className = 'mb-0'
-  p.innerText = quote.quote
-
-  const footer = document.createElement('footer')
-  footer.className = 'blockquote-footer'
-  footer.innerText = quote.author
-
-  const br = document.createElement('br')
-
-  const like_btn = document.createElement('button')
-  like_btn.className = 'btn-success'
-  like_btn.innerHTML = `Likes: <span>${0}</span>`
-
-  const delete_btn = document.createElement('button')
-  delete_btn.className = 'btn-danger'
-  delete_btn.innerText = 'Delete'
-
-  ul.appendChild(li)
-  li.appendChild(blockquote)
-  blockquote.appendChild(p)
-  blockquote.appendChild(footer)
-  blockquote.appendChild(br)
-  blockquote.appendChild( document.createElement('br') )
-  blockquote.appendChild(like_btn)
-  blockquote.appendChild(delete_btn)
-}
-
-// [6]
-function createQuote(e) { 
-  e.preventDefault()
-  // 리프레시 때 안올라가게 하는 것
-  const quote = quoteInput.value
-  const author = authorInput.value
-  const newQuote = {'quote': quote, 'author': author}
-  
-  makeQuote(newQuote)
-  e.target.reset()
-  addQuote(newQuote)
-}
-
-// [7] 
-function makeQuote(newQuote) {
-  fetch('http://localhost:3000/quotes', {
-    method: 'POST',
-    body: JSON.stringify(newQuote), 
-    headers:{
-      'Content-Type': 'application/json'
-    }
-  }).then(res => res.json())
-  .then(res => display_quote(res))
-}
-
